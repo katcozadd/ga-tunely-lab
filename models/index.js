c647a64d144e2eb2ec0f4c9df9f88548ef978c07
Jason Spiller <jason@thetargetlab.com>
2018-04-20 09:59:11 -0600

    Adding database.

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.MLAB_URL)
 } else {
 	console.log("this is the local ")
-  mongoose.connect("mongodb://localhost/todo-lab");
+  mongoose.connect("mongodb://localhost/rhapsodic");
 }
 
 
