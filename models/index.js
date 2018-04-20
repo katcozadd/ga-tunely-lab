d166b31480d3819791c60033d251ed43eaa7c6bb
Jason Spiller <jason@thetargetlab.com>
2018-04-20 10:46:26 -0600

    Fixed model bug.

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 if (process.env.NODE_ENV == "production") {
 	console.log("connecting to... " + process.env.NODE_ENV)
-	console.log("also connecting to mlab  " + MLAB_URL)
+	console.log("also connecting to mlab  " + process.env.MLAB_URL)
   mongoose.connect(process.env.MLAB_URL)
 } else {
 	console.log("this is the local ")
